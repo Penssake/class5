5e65c696a0dc0655d2e9f7467f4bc9ab56577ff9
Christina Thomas <Penssake@gmail.com>
2016-10-21 18:14:40 -0700

    Answer to question 4. Dope, I can feel my brain expanding.

--- question.js
+++ question.js
@@ -78,13 +78,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2,3,4];
 
-function sumArray(testArray){ //eslint-disable-line
-
+function sumArray(numArray){
+  var total = 0;
+  for(var i = 0; i < numArray.length; i++) {
+    total = sum(numArray[i], total);
+  }
+  return total;
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
